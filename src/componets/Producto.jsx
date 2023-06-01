30c0210696dc27d27f4a48d76470db2fbda77d15
Yordy Padilla <yjrpadilla@gmail.com>
2023-05-31 23:53:08 -0500

    uno mas

--- src/componets/Producto.jsx
+++ src/componets/Producto.jsx
@@ -44,21 +44,21 @@ const Producto = () => {
             <hr/> 
             <label for="mate">Material:</label>
             <select id="mate" onChange={(e) => setMaterialSeleccionado(e.target.value)}>
-            <option value="vac"> - </option>
+            <option value="vac">  - </option>
             <option value="cue">Cuero</option>
             <option value="crd">Cuerda</option>
             </select>
             <hr/>
             <label for="dije">Dije:</label>
             <select id="dije" onChange={(e) => setDijeSeleccionado(e.target.value)}>
-            <option value="vac"> - </option>
+            <option value="vac">  - </option>
             <option value="anc">Ancla</option>
             <option value="mar">Martillo</option>
             </select>
             <hr/>
             <label for="tipo">Tipo:</label>
             <select id="tipo" onChange={(e) => setTipoSeleccionado(e.target.value)}>
-            <option value="vac"> - </option>
+            <option value="vac">  - </option>
             <option value="oro">Oro</option>
             <option value="ros">Oro rosado</option>
             <option value="pla">Plata</option>
@@ -67,8 +67,8 @@ const Producto = () => {
             <hr/>
             <label for="pago">Metodo de pago:</label>
             <select id="pago" onChange={(e) => setPagoSeleccionado(e.target.value)}>
-            <option value="vac"> - </option>
-            <option value="dol">Dolar</option>
+            <option value="vac">  - </option>
+            <option value="dol">Dolares</option>
             <option value="pes">Pesos</option>
             </select>
      
